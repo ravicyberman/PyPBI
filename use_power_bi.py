a841db961ba906f4cee6c4339e42e6dd90fc2af4
ravicyberman <raviyerramilli91@gmail.com>
2022-01-22 21:53:29 -0500

    committing use_power_bi.py file

---
+++ use_power_bi.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from configparser import ConfigParser
+from use_power_bi.client import PowerBiClient 
+
+     
+config = ConfigParser()
+
+config.read('Configs/configs.ini')
+
+client_id = config.get('power_bi_app','client_id')
+client_secrect = config.get('power_bi_app','client_secret')
+
+# Init the PowerBi client 
+
+power_bi_client = PowerBiClient(
+
+client_id = client_id,
+client_secrect = client_secrect,
+credentials='Config/power_bi_state.jsonc'
+
+)
+

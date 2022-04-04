a0c2d406fa8d4249d981dce47e62677321a12556
Ravi Yerramilli <ravi.yerramilli@labatt.com>
2022-04-04 18:16:43 -0400

    commit on master

--- ImageBase64.py
+++ ImageBase64.py
@@ -1,11 +1,11 @@
 import base64
 from pathlib import Path
 
-loc = "D:\Ravi\eagle eye.png"      # Read file location 
+loc = "D:\Ravi\eagle eye.png"      # Read file location; replace with your file path.
 
 file_ext = Path(loc).suffix
 with open(loc, "rb") as img_file:
     Base64string = base64.b64encode(img_file.read())
 
-print("data:image/",file_ext,";base64",Base64string)                     # Build prefix to Base64 for it to work in Power BI for e.r: data:image/jpeg;base64
-
+print("data:image/",file_ext,";base64",Base64string.decode('utf-8'))                     # Build prefix to Base64 for it to work in Power BI for e.r: data:image/jpeg;base64
+                                                                                         # added decode('utf-8') to avoid prefix e.g: b'string' 
